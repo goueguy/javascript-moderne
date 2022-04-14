4c91ed222f7cc39e8425805951a6a7653e29633c
alexis225 <jean-louis.goueguy@groupensia.com>
2022-04-14 17:06:24 +0000

    variables

---
+++ script.js
@@ -0,0 +1,23 @@
+"use_strict"
+//VAR
+var first = "wes";
+//LET
+
+let age = 100;
+
+//CONST
+const  cool = true;
+
+
+if(age>10){;
+    console.log("YOU ARE OLD");
+};
+
+first="welcome";
+//cool = false;
+dogs = "fisher";
+console.log(first,age,cool,dogs);
+
+let addressEmail ="";
+let nomComplet;
+let nomDeJeuneFille
